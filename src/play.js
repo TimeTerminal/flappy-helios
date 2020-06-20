2f9f5fe7e1d86e328c6ed879926a2a3534dad4dc
Time_Terminal <vishesh.thanki@gmail.com>
2020-06-20 14:51:53 -0400

    Get reference to canvas in JS file, Add animate function

--- src/play.js
+++ src/play.js
@@ -1,8 +1,19 @@
-// import { SCORE, BEST } from "./constants.js";
+// import { SCORE, BEST, CANVAS_HEIGHT } from "./constants.js";
 
 const play = function () {
   // const score = SCORE;
   console.log("Game has started");
+  const canvas = document.getElementById("canvas");
+
+  canvas.width = 800;
+  canvas.height = 800;
+
 };
+
+const animate = function() {
+  // Creates an infinite loop - as the requestAnimationFrame function calls the animate function over and over
+  requestAnimationFrame(animate);
+}
+
 
 play();
