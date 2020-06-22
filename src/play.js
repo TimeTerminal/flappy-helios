ff1eff6e26b5942eb219f33aeb299b6e82f931fc
Time_Terminal <vishesh.thanki@gmail.com>
2020-06-21 23:51:51 -0400

    Improve styling, Add draw function

--- src/play.js
+++ src/play.js
@@ -1,19 +1,20 @@
 // import { SCORE, BEST, CANVAS_HEIGHT } from "./constants.js";
 
-const play = function () {
-  // const score = SCORE;
-  console.log("Game has started");
-  const canvas = document.getElementById("canvas");
+const draw = (function () {
+  const canvas = document.getElementById('canvas');
+  console.log('Drawing the canvas', canvas);
+  const ctx = canvas.getContext('2d');
 
-  canvas.width = 800;
+  canvas.width = 500;
   canvas.height = 800;
+})()
 
+const play = function () {
+  console.log("Game has started");
+  // const score = SCORE;
 };
 
-const animate = function() {
+const animate = function () {
   // Creates an infinite loop - as the requestAnimationFrame function calls the animate function over and over
   requestAnimationFrame(animate);
 }
-
-
-play();
